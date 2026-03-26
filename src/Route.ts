3104d31f75b179322844ab590594ca7df099774b
axtk <axtkx@inbox.ru>
2026-03-26 18:38:59 +0300

    mark only links with href as active

--- src/Route.ts
+++ src/Route.ts
@@ -38,7 +38,7 @@ function isLinkElement(x: unknown): x is LinkElement {
 function toggleActive(element: Element | Node, route: Route) {
   if (!isLinkElement(element)) return;
 
-  if (route.match(route.toValue(element.href)).ok)
+  if (element.hasAttribute("href") && route.match(route.toValue(element.href)).ok)
     element.dataset.active = "true";
   else delete element.dataset.active;
 }
